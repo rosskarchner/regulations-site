eb058ee284801c4caa62989543b1ab967e892018
Ross M Karchner <ross@karchner.com>
2017-02-01 11:02:21 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="regulations",
-    version="2.1.5",
+    version_format='{tag}.dev{commitcount}+{gitsha}',
     packages=find_packages(),
     include_package_data=True,
     setup_requires=['cfgov_setup==1.2',],
@@ -17,5 +17,6 @@ setup(
     classifiers=[
         'License :: Public Domain',
         'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication'
-    ]
+    ],
+    setup_requires=['setuptools-git-version'],
 )
